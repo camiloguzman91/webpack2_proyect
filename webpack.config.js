a0e77c380e9f22ac1b6666f5dbeced2d4935dc70
Camilo Guzman <camiloge_91@hotmail.com>
2021-03-06 12:38:32 -0500

    fonts_loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     // filename nombra al archivo final
     filename: 'main.js',
+    assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
   resolve: {
     //aquí se ponen las extensiones que tendrá el proyecto para que webpack los lea
@@ -38,6 +39,29 @@ module.exports = {
       {
         test: /\.png/,
         type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000, // Habilita o deshabilita la transformación de archivos en base64.
+            mimetype: "application/font-woff",
+            // Especifica el tipo MIME con el que se alineará el archivo. 
+            // Los MIME Types (Multipurpose Internet Mail Extensions)
+            // son la manera standard de mandar contenido a través de la red.
+            name: "[name].[ext]",
+            // EL NOMBRE INICIAL DEL ARCHIVO + SU EXTENSIÓN
+            // Se puede agregar [name]hola.[ext] y el output del archivo sería 
+            // ubuntu-regularhola.woff
+            outputPath: "./assets/fonts",
+            // EL DIRECTORIO DE SALIDA (SIN COMPLICACIONES)
+            publicPath: "./assets/fonts",
+            // EL DIRECTORIO PÚBLICO (SIN COMPLICACIONES)
+            esModule: false,
+            // AVISAR EXPLICITAMENTE SI ES UN MODULO
+          },
+        }
       }
     ]
   },
