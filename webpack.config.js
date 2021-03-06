9d859b569467c0bc6b43f9746d8bc2cbf6a09167
Camilo Guzman <camiloge_91@hotmail.com>
2021-03-05 23:45:04 -0500

    img_loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   //entry permite decir el punto de entrada de la aplicación
@@ -33,6 +34,10 @@ module.exports = {
           'css-loader',
           'stylus-loader'
         ]
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource'
       }
     ]
   },
@@ -43,5 +48,13 @@ module.exports = {
       filename: './index.html'
     }),
     new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images"
+        }
+      ]
+    })
   ]
 }
